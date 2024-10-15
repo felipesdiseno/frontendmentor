29eb110d1cafd13fdc337c3a911817863470e5f3
Felipe Sanchez <felipe.sdiseno@gmail.com>
2024-10-15 10:43:47 -0500

    cambios realizados en estilos

--- src/components/QrCard/index.tsx
+++ src/components/QrCard/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function QrCard() {
   return (
-    <div className="flex items-center flex-col w-[335px] h-[497px] rounded-xl bg-white shadow-xl p-4">
-      <div className="rounded-xl">
-        <img src="qr-code.png" alt="codigoQr" className="rounded-xl" />
+    <div className="flex items-center flex-col w-[320px] h-[499px] rounded-3xl bg-white shadow-xl p-4">
+      <div>
+        <img src="qr-code.png" alt="codigoQr" className="rounded-2xl" />
       </div>
       <div>
-        <p className="text-center font-bold text-xl">
+        <p className="text-center font-bold text-xl text-[#1f314f]">
           Improve your front-end skills by building projects
         </p>
         <p className="text-base text-center">

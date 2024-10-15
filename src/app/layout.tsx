c3a4ba56f02a4dff5ae887005232a93523cabee8
Felipe Sanchez <felipe.sdiseno@gmail.com>
2024-10-15 10:25:17 -0500

    creación de tarjeta codigo qr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="min-h-screen">{children}</div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import QrCard from "@/components/QrCard";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-centerfont-[family-name:var(--font-geist-sans)]">
-      <h1>Hello</h1>
+    <div className="flex items-center justify-center font-[family-name:var(--font-geist-sans)] flex-col bg-slate-200">
+      <h1>Projects carried out to complete the frontend mentor challenge </h1>
       <QrCard />
     </div>
   );

2208acf230b79e1f1570c3a95b55d54f78f67e4d
Felipe Sanchez <felipe.sdiseno@gmail.com>
2024-10-16 09:20:27 -0500

    se añadio foto personal a la card

--- src/components/ProfileLinksCard/index.tsx
+++ src/components/ProfileLinksCard/index.tsx
@@ -5,36 +5,35 @@ function ProfileLinkCard() {
   return (
     <div className="flex flex-col rounded-md  bg-gray-800 p-6">
       <img
-        src="profilelink/jessica.jpeg"
+        src="profilelink/profilepic.jpg"
         alt="profile picture"
-        className="self-center rounded-full"
+        className="self-center rounded-full w-56 h-56"
       />
       <h1 className="text-2xl font-bold text-white text-center">
         Felipe Sanchez
       </h1>
-      <h2 className="text-sm text-green-500 text-center mt-4">
-        {" "}
+      <h2 className="text-sm text-[#C8F22E] text-center mt-4">
         Palmira, Colombia
       </h2>
-      <h2 className="text-sm text-green-500 text-center mt-4">
+      <h2 className="text-sm text-white text-center mt-4">
         &quot;Front-end Developer and industrial designer&quot;
       </h2>
       <div className="flex flex-col justify-center gap-4 mt-4">
-        <Button className="bg-gray-700 hover:bg-[#4B5E39]">
+        <Button className="bg-gray-700 hover:bg-[#C8F22E] hover:text-[#465902]">
           <Link href="https://github.com/felipesdiseno">GitHub</Link>
         </Button>
-        <Button className="bg-gray-700 hover:bg-[#4B5E39]">
+        <Button className="bg-gray-700 hover:bg-[#C8F22E] hover:text-[#465902]">
           <Link href="">Frontend Mentor</Link>
         </Button>
-        <Button className="bg-gray-700 hover:bg-[#4B5E39]">
+        <Button className="bg-gray-700 hover:bg-[#C8F22E] hover:text-[#465902]">
           <Link href="https://www.linkedin.com/in/felipe-frontdev/">
             Linkedin
           </Link>
         </Button>
-        <Button className="bg-gray-700 hover:bg-[#4B5E39]">
+        <Button className="bg-gray-700 hover:bg-[#C8F22E] hover:text-[#465902]">
           <Link href="https://x.com/?lang=es">Twitter</Link>
         </Button>
-        <Button className="bg-gray-700 hover:bg-[#4B5E39]">
+        <Button className="bg-gray-700 hover:bg-[#C8F22E] hover:text-[#465902]">
           <Link href="https://www.instagram.com/">Instagram</Link>
         </Button>
       </div>

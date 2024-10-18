222c407f87d35ab94b8be98c323305aa8a3dce8e
Felipe Sanchez <felipe.sdiseno@gmail.com>
2024-10-18 11:53:56 -0500

    cambios realizados en estilos de recipeComponent

--- src/app/recipepage/page.tsx
+++ src/app/recipepage/page.tsx
@@ -4,15 +4,13 @@ import { IoHome } from "react-icons/io5";
 import Link from "next/link";
 function RecipePafe() {
   return (
-    <div className="bg-[#f3e5d8] flex items-center justify-center flex-col p-6">
+    <div className="bg-[#f3e5d8] flex items-center justify-center p-2">
       <div className="absolute top-4 right-6 p-2 rounded-full bg-gray-400 text-white  hover:bg-gray-500 hover:cursor-pointer transform hover:scale-110 ease-in-out">
         <Link href="/">
-          <IoHome className="text-3xl e" />
+          <IoHome className="text-3xl" />
         </Link>
       </div>
-      <h1 className="text-3xl font-bold mb-4 text-gray-500">
-        A wonderful Recipe
-      </h1>
+
       <RecipeComponent />
     </div>
   );

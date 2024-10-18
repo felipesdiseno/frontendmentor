c47da1f0c39dde3835a4bf8a8053facc7a574568
Felipe Sanchez <felipe.sdiseno@gmail.com>
2024-10-18 15:31:17 -0500

    se creo el componente Testimonial component y la page que lo renderiza

---
+++ src/app/testimonialpage/page.tsx
@@ -0,0 +1,12 @@
+import TestimonialGridComponent from "@/components/TestimonialGrid";
+import React from "react";
+
+function TestimonialGridPage() {
+  return (
+    <div className="flex items-center justify-center p-2 bg-[#edf2f8]">
+      <TestimonialGridComponent />
+    </div>
+  );
+}
+
+export default TestimonialGridPage;

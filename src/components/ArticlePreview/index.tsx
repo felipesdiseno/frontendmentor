3b0b242aa6fd458702cb30cc8cef8d8185edef42
Felipe Sanchez <felipe.sdiseno@gmail.com>
2024-10-23 15:12:58 -0500

    se corrigieron estilos en version movile

--- src/components/ArticlePreview/index.tsx
+++ src/components/ArticlePreview/index.tsx
@@ -1,29 +1,40 @@
 "use client";
+
 import React, { useState } from "react";
 import { IoMdShareAlt } from "react-icons/io";
 import SharePop from "@/components/ArticlePreview/sharePop";
 
 function ArticlePreviewComponent() {
   const [sharePop, setSharePop] = useState(false);
+
+  const handleShareClick = () => setSharePop(!sharePop);
+
   return (
-    <div className="flex flex-col md:flex-row rounded-xl shadow-xl relative w-full max-w-[730px]">
+    <div className="flex flex-col md:flex-row rounded-xl shadow-xl relative w-full max-w-[730px] pb-[120px] md:pb-0">
       <img
         src="articlePreview/drawers.jpg"
         alt="drawers"
         className="md:rounded-l-xl w-full h-auto md:w-[50%]"
       />
-      <div className="flex flex-col p-4 md:ml-6">
-        <p className="font-bold text-xl md:text-2xl cursor-default text-[#48556A] font-manrope mb-4">
-          Shift the overall look and feel by adding these wonderful touches to
-          furniture in your home
-        </p>
-        <p className="text-sm md:text-base cursor-default text-[#6D7F97] mb-4">
-          Ever been in a room and felt like something was missing? Perhaps it
-          felt slightly bare and uninviting. I’ve got some simple tips to help
-          you make any room feel complete.
-        </p>
-        <div className="flex flex-row items-center justify-between mt-4 ">
-          <div className="flex flex-row items-center mb-4 md:mb-0">
+      <div className="flex flex-col p-4 md:p-8 w-full relative">
+        <div>
+          <p className="font-bold text-xl md:text-2xl cursor-default text-[#48556A] font-manrope mb-4">
+            Shift the overall look and feel by adding these wonderful touches to
+            furniture in your home
+          </p>
+          <p className="text-sm md:text-base cursor-default text-[#6D7F97] mb-4">
+            Ever been in a room and felt like something was missing? Perhaps it
+            felt slightly bare and uninviting. I&apos;ve got some simple tips to
+            help you make any room feel complete.
+          </p>
+        </div>
+
+        <div
+          className={`flex flex-row items-center justify-between ${
+            sharePop ? "md:flex hidden" : "flex"
+          }`}
+        >
+          <div className="flex flex-row items-center">
             <img
               src="articlePreview/avatar-michelle.jpg"
               alt="avatar"
@@ -38,16 +49,37 @@ function ArticlePreviewComponent() {
               </h3>
             </div>
           </div>
-          <div className="relative">
+
+          <div className="relative z-10">
             <div
-              className="bg-[#9EAFC2] rounded-full p-2 hover:bg-[#6E8098] cursor-pointer"
-              onClick={() => setSharePop(!sharePop)}
+              className={`rounded-full p-2 cursor-pointer ${
+                sharePop ? "bg-[#6E8098]" : "bg-[#9EAFC2] hover:bg-[#6E8098]"
+              }`}
+              onClick={handleShareClick}
             >
               <IoMdShareAlt className="text-white" />
             </div>
-            {sharePop && <SharePop />}
           </div>
         </div>
+
+        {sharePop && (
+          <div className="md:hidden fixed bottom-0 left-0 right-0 bg-[#48556A] py-4 px-8 flex items-center justify-between z-50">
+            <SharePop />
+
+            <div
+              className="bg-[#6E8098] rounded-full p-2 cursor-pointer"
+              onClick={handleShareClick}
+            >
+              <IoMdShareAlt className="text-white" />
+            </div>
+          </div>
+        )}
+
+        {sharePop && (
+          <div className="hidden md:block absolute -top-16 right-0">
+            <SharePop />
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/ArticlePreview/sharePop.tsx
+++ src/components/ArticlePreview/sharePop.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { FaFacebookSquare, FaTwitter, FaPinterest } from "react-icons/fa";
 import Link from "next/link";
+
 function SharePop() {
   return (
-    <div className="absolute bottom-1 left-1/2 transform -translate-x-1/2 -translate-y-full bg-[#48556A] p-4 rounded-xl shadow-md flex flex-row items-center justify-center   w-[248px] h-[55px]">
+    <div className="absolute md:bottom-1 bottom-0 left-1/2 transform -translate-x-1/2 md:-translate-y-full bg-[#48556A] md:p-4 p-0 rounded-xl shadow-md flex flex-row items-center justify-center w-full md:w-[248px] h-[55px]">
       <div className="flex flex-row gap-2 justify-between">
         <h1 className="text-white text-[13px] tracking-widest font-manrope">
           SHARE
@@ -20,8 +21,8 @@ function SharePop() {
           </Link>
         </div>
       </div>
-      {/* La punta de diálogo */}
-      <div className="absolute w-5 h-5 bg-[#48556A] transform rotate-45 -bottom-2  flex items-center"></div>
+
+      <div className="absolute w-5 h-5 bg-[#48556A] transform rotate-45 -bottom-2 md:flex items-center hidden"></div>
     </div>
   );
 }

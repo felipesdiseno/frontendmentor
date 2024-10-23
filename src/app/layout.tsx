45dbd8873d2f24937eb5aac33305ea968e9b74f6
Felipe Sanchez <felipe.sdiseno@gmail.com>
2024-10-23 14:41:14 -0500

    imagenes agregadas, cambios en page home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="favicon-32x32.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- src/app/recipepage/page.tsx
+++ src/app/recipepage/page.tsx
@@ -4,7 +4,7 @@ import { IoHome } from "react-icons/io5";
 import Link from "next/link";
 function RecipePafe() {
   return (
-    <div className="bg-[#f3e5d8] flex items-center justify-center p-2">
+    <div className="bg-[#f3e5d8] flex items-center justify-center p-6">
       <div className="absolute top-4 right-6 p-2 rounded-full bg-gray-400 text-white  hover:bg-gray-500 hover:cursor-pointer transform hover:scale-110 ease-in-out">
         <Link href="/">
           <IoHome className="text-3xl" />

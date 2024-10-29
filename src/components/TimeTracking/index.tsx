14eca3b1cb3b8e5bdf586c25a5759334961d09fb
Felipe Sanchez <felipe.sdiseno@gmail.com>
2024-10-29 15:17:13 -0500

    se agrego la tipografia rubik

--- src/components/TimeTracking/index.tsx
+++ src/components/TimeTracking/index.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 function TimeTRackingC() {
   const [selected, setSelected] = useState("daily");
   return (
-    <div className="flex md:flex-row sm:flex-col gap-6 md:w-[160px] md:h-[272px] sm:w-[240px] sm:h-[74.66px]">
+    <div className="flex md:flex-row sm:flex-col gap-6 md:w-[160px] md:h-[272px] sm:w-[240px] sm:h-[74.66px] ">
       <div className="flex flex-col bg-[#1C1F4A] rounded-xl">
         <div className="bg-[#5847EB] rounded-xl p-2 md:flex-col sm:flex-row flex md:w-[160px] md:h-[181px] sm:w-[240px] sm:h-[74.66px]">
           <div className="p-1 bg-white rounded-full flex  items-start w-fit">
@@ -15,7 +15,7 @@ function TimeTRackingC() {
             />
           </div>
           <h3>Report for</h3>
-          <h2>Jeremy Robson</h2>
+          <h2 className="text-white text-2xl font-rubik">Jeremy Robson</h2>
         </div>
         <div className="flex md:flex-col sm:flex-row gap-2 sm:justify-between sm:items-center md:items-start p-2">
           <h3
